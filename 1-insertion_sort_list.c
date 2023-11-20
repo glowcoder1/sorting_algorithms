4e5b5eedacb93a1f0e64ec97e8013c7311e79aed
glowcoder1 <tefeglory.official@gmail.com>
2023-11-20 07:17:04 -0800

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,9 +8,13 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr = *list;
+	listint_t *curr = NULL;
 	listint_t *temp = NULL;
 
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+		return;
+
+	curr = *list;
 	curr = curr->next;
 	while (curr)
 	{
